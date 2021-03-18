12b2d4162d283e5e0831f15165ec55e715f1cea3
usman37tariq <37794545+usman37tariq@users.noreply.github.com>
2021-03-18 15:25:14 +0500

    Update pulldocker.sh

--- scripts/pulldocker.sh
+++ scripts/pulldocker.sh
@@ -1,3 +1,1 @@
-echo Starting docker pull 
-echo $IMAGE_TAG
-sudo docker pull 422193507691.dkr.ecr.us-east-1.amazonaws.com/paperless-aws:$IMAGE_TAG
+sudo docker pull 422193507691.dkr.ecr.us-east-1.amazonaws.com/paperless-aws:latest
