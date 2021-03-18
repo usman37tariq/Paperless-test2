0f1e6dc70f522cd2db134539262ce43248f5b88e
usman37tariq <37794545+usman37tariq@users.noreply.github.com>
2021-03-18 17:47:35 +0500

    Update pulldocker.sh

--- scripts/pulldocker.sh
+++ scripts/pulldocker.sh
@@ -1,3 +1,4 @@
-IMAGE_NAME=$(head -n 1 /home/ubuntu/artifacts/imagename.txt)
-echo ${IMAGE_NAME}
-sudo docker pull ${IMAGE_NAME}
+#IMAGE_NAME=$(head -n 1 /home/ubuntu/artifacts/imagename.txt)
+#echo ${IMAGE_NAME}
+#sudo docker pull ${IMAGE_NAME}
+echo "pull docker file execution"
