f7da882ea5054e81768c3195c825e0129d43f0f2
usman37tariq <37794545+usman37tariq@users.noreply.github.com>
2021-03-18 18:08:01 +0500

    Update pulldocker.sh

--- scripts/pulldocker.sh
+++ scripts/pulldocker.sh
@@ -1,3 +1,5 @@
-IMAGE_NAME=$(head -n 1 /home/ubuntu/artifacts/imagename.txt)
+ARTIFACTS_DIR=/home/ubuntu/artifacts
+IMAGE_NAME=$(head -n 1 ${ARTIFACTS_DIR}/imagename.txt)
 echo ${IMAGE_NAME}
 sudo docker pull ${IMAGE_NAME}
+sudo rm -r ${ARTIFACTS_DIR}/*
