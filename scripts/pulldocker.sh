89dab6194d5df7ffc804f22eb6e4939e71d775c8
usman37tariq <37794545+usman37tariq@users.noreply.github.com>
2021-03-18 16:57:41 +0500

    Add files via upload

--- scripts/pulldocker.sh
+++ scripts/pulldocker.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-sudo IMAGE_NAME=$(head -n 1 /home/ubuntu/artifacts/imagename.txt)
-sudo docker pull $IMAGE_NAME
+IMAGE_NAME=$(head -n 1 imagename.txt)
+echo ${IMAGE_NAME}
+sudo docker pull ${IMAGE_NAME}
